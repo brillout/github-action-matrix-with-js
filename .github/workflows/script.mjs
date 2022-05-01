0bccfe2fb85b1e07b451d03e8e42f2aaa8a30856
Romuald Brillout <git@brillout.com>
2022-05-01 07:32:02 +0200

    wip

--- .github/workflows/script.mjs
+++ .github/workflows/script.mjs
@@ -3,7 +3,7 @@
 let tests = (await quiet($`ls examples/`)).stdout.split(/\s/).filter(Boolean)
 
 const matrixData = tests.map(test => {
-  return { project: test, config: 'bl' }
+  return { project: test, config: 'bla2' }
 })
 
 console.log(`{"include":${JSON.stringify(matrixData)}}`)
