f90148245898668224b9d438a14460837a2a3951
Romuald Brillout <git@brillout.com>
2022-05-01 07:24:46 +0200

    wip

--- .github/workflows/path.mjs
+++ .github/workflows/path.mjs
@@ -1,7 +1,11 @@
 #!/usr/bin/env zx
 
+console.log(__dirname)
 console.log(await quiet($`ls`))
+console.log(await quiet($`pwd`))
+/*
 console.log(await quiet($`ls ./`))
 console.log(await quiet($`ls ../`))
 console.log(await quiet($`ls ../../`))
 console.log(await quiet($`ls ../../examples/`))
+*/
